1c5b10128d7f032c6b1f3d3efc55da2f0ad72eff
awhite2 <awhite2@gmail.com>
2021-04-14 15:23:04 +0100

    Images

---
+++ Final.js
@@ -0,0 +1,25 @@
+//object takes:
+//photo of cleopatra
+//location of "heart"
+//orientation
+//name of artist
+//position of origin height-height of image etc
+
+let portrait1, portrait2, portrait3, portrait4, portrait5, portrait6, portrait7;
+
+
+function preload(){
+
+
+}
+
+function setup(){
+
+
+}
+
+function draw(){
+
+
+}
+
